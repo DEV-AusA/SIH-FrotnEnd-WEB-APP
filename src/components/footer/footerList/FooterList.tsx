a3953540292099d2507fa10da03bf6714b910465
Alejandro <a-a-m2001@hotmail.com>
2024-05-08 14:31:18 -0500

    footer links added

--- src/components/footer/footerList/FooterList.tsx
+++ src/components/footer/footerList/FooterList.tsx
@@ -1,25 +1,24 @@
+import links from "@/helpers/links";
+import Link from "next/link";
+
 const FooterList: React.FC = (): React.ReactElement => {
   return (
     <ul className="flex items-center justify-center px-16">
-      <li className="text-xs text-[#FFBD5C] hover:text-[#ffffff] mx-5">
-        Inicio
-      </li>
-      <span>|</span>
-      <li className="text-xs text-[#FFBD5C] hover:text-[#ffffff] mx-5">
-        Administrador
-      </li>
-      <span>|</span>
-      <li className="text-xs text-[#FFBD5C] hover:text-[#ffffff] mx-5">
-        Propietarios
-      </li>
-      <span>|</span>
-      <li className="text-xs text-[#FFBD5C] hover:text-[#ffffff] mx-5">
-        Sobre nosotros
-      </li>
-      <span>|</span>
-      <li className="text-xs text-[#FFBD5C] hover:text-[#ffffff] mx-5">
-        Contacto
-      </li>
+      {links.map((link) => {
+        return (
+          <>
+            {link !== links[0] ? <span>|</span> : ""}
+
+            <Link
+              href={link.href}
+              key={link.name}
+              className="text-xs text-[#FFBD5C] hover:text-[#ffffff] mx-5"
+            >
+              {link.text}
+            </Link>
+          </>
+        );
+      })}
     </ul>
   );
 };
