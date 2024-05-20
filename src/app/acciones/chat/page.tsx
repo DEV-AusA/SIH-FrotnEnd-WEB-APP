54d74954eafb326c2f42b4fba1027cd64fb6c628
Alejandro <a-a-m2001@hotmail.com>
2024-05-20 14:43:24 -0500

    chat

---
+++ src/app/acciones/chat/page.tsx
@@ -0,0 +1,11 @@
+import ChatComponent from "@/components/chat/ChatComponent";
+
+const ChatPage = () => {
+  return (
+    <main>
+      <ChatComponent />
+    </main>
+  );
+};
+
+export default ChatPage;
