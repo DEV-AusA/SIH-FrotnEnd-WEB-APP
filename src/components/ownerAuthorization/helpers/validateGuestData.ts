246f3f1c8772e32447fdb2597d56b562d6f2e007
Alejandro <a-a-m2001@hotmail.com>
2024-05-27 22:23:52 -0500

    Validations fixed

--- src/components/ownerAuthorization/helpers/validateGuestData.ts
+++ src/components/ownerAuthorization/helpers/validateGuestData.ts
@@ -1,5 +1,7 @@
 import { IAuthorization } from "@/helpers/types";
 
+const onlypositivenumbers = /^[0-9]+$/;
+const onlyLetters = /^[a-zA-Z\s]+$/;
 const validateGuestData = ({ name, document }: IAuthorization) => {
   const errors: IAuthorization = {
     type: "",
@@ -9,9 +11,14 @@ const validateGuestData = ({ name, document }: IAuthorization) => {
   if (!name) errors.name = "Ingresa el nombre del invitado.";
   else if (name.trim().length < 3 || name.trim().length > 30) {
     errors.name = "Nombre invalido.";
-  } else if (!document) errors.document = "Ingresa el documento del invitado.";
-  else if (document.trim().length !== 8) {
+  } else if (!onlyLetters.test(name)) {
+    errors.name = "Solo se permiten letras.";
+  } else if (!document) {
+    errors.document = "Ingresa el documento del invitado.";
+  } else if (document.trim().length !== 8) {
     errors.document = "Documento invalido.";
+  } else if (!onlypositivenumbers.test(document)) {
+    errors.document = "Este campo solo admite números.";
   }
   return errors;
 };
--- src/components/updateForm/helpers/validateUpdate.ts
+++ src/components/updateForm/helpers/validateUpdate.ts
@@ -1,9 +1,10 @@
 import { IRegister } from "../../../helpers/types";
 
-const emailRegExp = /\S+@\S+\.\S+/;
+const emailRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const userRegExp = /^[a-zA-Z0-9]+$/;
 const passwordRegExp =
   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W)(?!.*\s).{8,15}$/;
+const onlypositivenumbers = /^[0-9]+$/;
 
 const validateUpdate = ({
   name,
@@ -28,13 +29,17 @@ const validateUpdate = ({
     password: "",
     confirmpassword: "",
   };
-  if (name && (name.trim().length < 2 || name.trim().length > 30))
-    errors.name = "Nombre Invalido.";
-  else if (
-    lastName &&
-    (lastName.trim().length < 2 || lastName.trim().length > 30)
-  ) {
+  if (!name) errors.name = "Ingresa tu nombre.";
+  else if (name.trim().length < 2 || name.trim().length > 30) {
+    errors.name = "Nombre invalido.";
+  } else if (!userRegExp.test(name)) {
+    errors.name = "Este campo no acepta caracteres especiales.";
+  } else if (!lastName) {
+    errors.lastName = "Ingresa tu apellido.";
+  } else if (lastName.trim().length < 2 || lastName.trim().length > 30) {
     errors.lastName = "Apellido invalido.";
+  } else if (!userRegExp.test(lastName)) {
+    errors.lastName = "Este campo no acepta caracteres especiales.";
   } else if (email && !emailRegExp.test(email))
     errors.email = "Correo electrónico invalido.";
   else if (
@@ -42,17 +47,27 @@ const validateUpdate = ({
     (!userRegExp.test(username) || username.trim().length < 3)
   ) {
     errors.username = "Usuario invalido.";
-  } else if (document && document.trim().length !== 8) {
-    errors.document = "Documento Invalido.";
-  } else if (cellphone && cellphone.trim().length < 10)
-    errors.cellphone = "Número de telefono invalido.";
-  else if (password && !passwordRegExp.test(password))
+  } else if (!document) {
+    errors.document = "Ingresa tu documento.";
+  } else if (document.trim().length !== 8) {
+    errors.document = "Documento invalido.";
+  } else if (!onlypositivenumbers.test(document)) {
+    errors.document = "Este campo solo admite números.";
+  } else if (!cellphone) errors.cellphone = "Ingresa tu telefono.";
+  else if (cellphone.trim().length < 10) {
+    errors.cellphone = "Numero de telefono invalido";
+  } else if (!onlypositivenumbers.test(cellphone)) {
+    errors.cellphone = "Este campo solo admite números.";
+  } else if (password && !passwordRegExp.test(password))
     errors.password =
       "Almenos: una mayúscula, una minúscula, un carácter especial y un número.";
   else if (confirmpassword !== password)
     errors.confirmpassword = "La contraseña no coincide.";
-  else if (phone && phone.trim().length < 10 && phone !== "0")
-    errors.phone = "Número de telefono invalido.";
+  else if (phone !== "0" && phone.trim().length < 10) {
+    errors.phone = "Numero de telefono invalido";
+  } else if (phone !== "0" && !onlypositivenumbers.test(phone)) {
+    errors.phone = "Este campo solo admite números.";
+  }
 
   return errors;
 };
