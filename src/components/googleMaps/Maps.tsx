8f97ff1f23e07c988704567c8e6b6ac22677f225
ManuelGuaicaraDagger <manuelgd1997@gmail.com>
2024-05-29 18:29:04 -0300

    Google maps

--- src/components/googleMaps/Maps.tsx
+++ src/components/googleMaps/Maps.tsx
@@ -1,49 +1,35 @@
 "use client";
 
-import { Loader } from "@googlemaps/js-api-loader";
-import React, { useEffect, useRef } from "react";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export function Maps() {
-  const mapRef = useRef<HTMLDivElement>(null);
-
+  const [text, setText] = useState<string | undefined>(undefined);
   useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-      version: "weekly",
-    });
-
-    async function initMap() {
-      await loader.importLibrary("maps");
-      await loader.importLibrary("marker");
-
-      if (mapRef.current) {
-        const map = new google.maps.Map(mapRef.current, {
-          center: { lat: -32.920546, lng: -68.806261 },
-          zoom: 15,
-          mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID as string,
-        });
-
-        const position = { lat: -32.920546, lng: -68.806261 };
-        new google.maps.marker.AdvancedMarkerElement({
-          position,
-          map,
-          title: "Mi Ubicación",
-        });
-      }
-    }
-
-    initMap().catch(console.error);
+    const getData = async () => {
+      const storedToken = localStorage.getItem("token");
+      const response = await axios.get(`${API_URL}/establishment`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      });
+      setText(response.data[0]?.location);
+    };
+    getData();
   }, []);
-
   return (
     <div className="flex flex-col justify-center items-center">
       <h2 className="text-[#384B59] text-4xl font-bold text-center max-[1400px]:text-2xl max-[800px]:text-xl mb-[50px]">
         Ubicación del barrio
       </h2>
-      <div
-        className="h-[500px] w-[70%] border-4 border-white mb-[50px] rounded-[10px]"
-        ref={mapRef}
-      ></div>
+      <div className="h-[500px] w-[70%] border-4 border-white mb-[50px] rounded-[10px]">
+        <iframe
+          src={text}
+          width="100%"
+          height="100%"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
   );
 }
